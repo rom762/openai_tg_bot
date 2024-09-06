205b19b96d6fc600190294977afb4b16406baceb
roman <nickro@ya.ru>
2024-09-06 22:14:33 +0300

    Add conversion youtube video to mp3

--- openai_bot.py
+++ openai_bot.py
@@ -2,6 +2,8 @@ import logging
 from telegram.ext import CommandHandler, MessageHandler, ApplicationBuilder, filters
 from openai import OpenAI
 import settings
+import yt_dlp as youtube_dl
+from youtube_dwnld import download_mp3
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -14,32 +16,56 @@ async def start(update, context):
     context.chat_data.clear()
     await update.message.reply_text('Hi! I am your AI bot. Ask me anything!')
 
+async def send_audio(update, context):
+    user_request = ' '.join(context.args).strip()
+    logging.debug(user_request)
+    filename = download_mp3(user_request)
+
+    with open(filename, 'rb') as af:
+       await context.bot.send_audio(chat_id=update.effective_chat.id, audio=af)
 
 async def handle_message(update, context):
+
     await context.bot.send_chat_action(chat_id=update.effective_chat.id, action='typing')
     message = update.message.text
-    messages = context.chat_data.get('messages', [])
-    messages.append({"role": "user", "content": message})
-    response = client.chat.completions.create(
-        model="gpt-4o",
-        messages=[{
-            "role": "system",
-            "content": "You are a helpful assistant."}, ] + messages
-    )
+    logging.info(f'User message:{message}')
+    if "https://www.youtube.com" in message:
+        await context.bot.send_message(update.effective_chat.id, "Please wait...")
+        video_info = youtube_dl.YoutubeDL().extract_info(url = message, download=False)
+        filename = f"{video_info['title']}.mp3"
+        options={
+            'format':'bestaudio/best',
+            'keepvideo':False,
+            'outtmpl':filename,
+        }
+        with youtube_dl.YoutubeDL(options) as ydl:
+            ydl.download([video_info['webpage_url']])
 
-    bot_response = response.choices[0].message.content
-    messages.append({"role": "assistant", "content": bot_response})
-    context.chat_data['messages'] = messages
-
-    tokens = response.usage.total_tokens 
-    logging.debug(f"current response tokens: {tokens}")
-
-    if 'total_tokens' not in context.chat_data:
-        context.chat_data['total_tokens'] = tokens
+        logging.info("Download complete... {}".format(filename))
+        await context.bot.send_audio(chat_id=update.effective_chat.id, audio=open(filename, 'rb'))
     else:
-        context.chat_data['total_tokens'] += tokens
+        messages = context.chat_data.get('messages', [])
+        messages.append({"role": "user", "content": message})
+        response = client.chat.completions.create(
+            model="gpt-4o",
+            messages=[{
+                "role": "system",
+                "content": "You are a helpful assistant."}, ] + messages
+        )
 
-    await update.message.reply_text(bot_response)
+        bot_response = response.choices[0].message.content
+        messages.append({"role": "assistant", "content": bot_response})
+        context.chat_data['messages'] = messages
+
+        tokens = response.usage.total_tokens 
+        logging.debug(f"current response tokens: {tokens}")
+
+        if 'total_tokens' not in context.chat_data:
+            context.chat_data['total_tokens'] = tokens
+        else:
+            context.chat_data['total_tokens'] += tokens
+
+        await update.message.reply_text(bot_response)
 
 async def stats(update, context):
     messages = context.chat_data.get('messages', [])
@@ -54,6 +80,7 @@ def main():
     
     application.add_handler(CommandHandler(["start", "help"], start))
     application.add_handler(CommandHandler(["stats",], stats))
+    application.add_handler(CommandHandler(["file", "music"], send_audio))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND,
                                            handle_message))
     application.run_polling()
---
+++ youtube_dwnld.py
@@ -0,0 +1,31 @@
+import yt_dlp as youtube_dl
+import os
+
+
+def is_exist(fname):
+    return os.path.isfile(fname)
+
+
+def download_mp3(url):
+    video_info = youtube_dl.YoutubeDL().extract_info(url=url, download=False)
+    filename = f"{video_info['title']}.mp3"
+    print(filename)
+    if not is_exist(filename):    
+        options={
+              'format':'bestaudio/best',
+              'keepvideo':False,
+              'outtmpl':filename,
+        }
+
+    try:
+        with youtube_dl.YoutubeDL(options) as ydl:
+            ydl.download([video_info['webpage_url']])
+    except:
+        result=f"ERROR, can't download this url"
+
+    return filename
+
+
+if __name__ == "__main__":
+    url = "https://www.youtube.com/watch?v=6pqOZq9eBBg"
+    download_mp3(url)
